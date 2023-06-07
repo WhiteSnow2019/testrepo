5a93532a17fc3f1898e6670b9e21b62887545230
WhiteSnow2019 <135812319+WhiteSnow2019@users.noreply.github.com>
2023-06-07 09:47:28 +0200

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Data Science Tools and Ecosystems
+
+# In this notebook Data Science Tools and Ecosystems are summarized
+
+# Some of the popular languages that data scientists use are:
+
+# In[2]:
+
+
+destinations=['R','phyton','c++']
+for i,dest in enumerate(destinations,1):
+    print("%d. %s" % (i,dest))
+
+
+# Some of the commonly libraries used by Data Scientists include:
+
+# In[3]:
+
+
+destinations=['TensorFlow','Numpy','Scipy']
+for i,dest in enumerate(destinations,1):
+    print("%d. %s" % (i,dest))
+
+
+# In[4]:
+
+
+destinations=['Data Science Tools','Jupiter Notebook','Visual Studio','Google Colab']
+for i in range(0,len(destinations)):
+    print(destinations[i])
+
+
+# Below are Examples of evaluating arithmetic expression in Phyton
+
+# This is a simple arithmetic expression to multiply then add integers
+
+# In[5]:
+
+
+(3*4)+5
+
+
+# This will convert 200 minutes to hours by dividing by 60
+
+# In[6]:
+
+
+200/60
+
+
+# In this notebook, Data Science Tools and Ecosystem are summarized
+
+# Objectives:
+
+# In[13]:
+
+
+a=['R','phyton','C++']
+for i in range(len(a)):
+    print("* " + a[i])
+
+
+# Author
+
+# Marco Gatta
